f3b61ea7049ec7f170e988ebb049a9411a72c48f
Ethan Haynes <ethanhaynes@Ethans-MacBook-Pro.local>
2017-11-11 19:53:56 -0600

    adding new blocks on each request

--- blocks/__main__.py
+++ blocks/__main__.py
@@ -4,8 +4,8 @@ import sys
 
 def main():
     try:
-        from .server import start
-        sys.exit(start())
+        from .server import run
+        sys.exit(run())
     except KeyboardInterrupt:
         from . import ExitStatus
         sys.exit(ExitStatus.ERROR_CTRL_C)
--- blocks/server.py
+++ blocks/server.py
@@ -1,11 +1,50 @@
-import http.server
-import socketserver
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SocketServer import ThreadingMixIn
+import threading, json, time, hashlib, random
 
-def start():
-    PORT = 8000
+Block = lambda index, previousBlock, previousHash, timestamp, data: {
+    'index' : index,
+    'previousBlock' : previousBlock,
+    'previousHash' : str(previousHash),
+    'timestamp' : timestamp,
+    'data' : data,
+    'hash' : hashlib.sha256(str(random.random())).hexdigest()
+}
 
-    Handler = http.server.SimpleHTTPRequestHandler
+getGenesisBlock = lambda : Block(0, 0, '0', int(time.time()), 'genesis block')
 
-    with socketserver.TCPServer(('127.0.0.1', PORT), Handler) as httpd:
-        print('serving at port', PORT)
-        httpd.serve_forever()
+getNextBlock = lambda blockData: Block(
+    blockData['index'] + 1,
+    blockData['previousBlock'] + 1,
+    blockData['hash'],
+    int(time.time()),
+    "new data" + str(blockData['index'] + 1)
+)
+
+blocks = [getGenesisBlock()]
+
+class Handler(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self.blockchain = blocks
+        return BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+
+    def do_GET(self):
+        print self.path
+        self.send_response(200)
+        self.end_headers()
+        message =  threading.currentThread().getName()
+
+        self.blockchain.append(getNextBlock(self.blockchain[-1:][0]))
+
+        self.wfile.write(json.dumps({ 'blockchain': list(map(lambda b: str(b), self.blockchain)) }))
+        self.wfile.write('\n')
+        return
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    '''Handle requests in a separate thread.'''
+
+def run():
+    PORT = 8080
+    server = ThreadedHTTPServer(('localhost', 8080), Handler)
+    print 'Starting server', PORT
+    server.serve_forever()
